c3da250f397c9e15c8363233a455bb15b9aa741f
SHASHANK SHARMA <shashanksharma@SHASHANKs-MacBook-Air.local>
2025-07-14 04:01:05 +0530

    modified the hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,13 @@
-print("hello world")
+# I want to print the table of 2 using loop. 
+
+# remove the while loop and use for loop.
+
+for i in range(1, 11):
+    print(f"2 * {i} = {2 * i}")
+
+
+# I want to print the table of 2 using while loop.
+
+i = 1
+while i <= 10:
+    print(f"2 * {i} = {2 * i}")
